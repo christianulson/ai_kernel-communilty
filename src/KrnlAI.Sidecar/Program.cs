9528d37f153ca240998975195407815917ab09a1
LAP-CHRIS\chris <christian_ulson@hotmail.com>
2026-06-16 09:54:40 -0300

    .

--- src/KrnlAI.Sidecar/Program.cs
+++ src/KrnlAI.Sidecar/Program.cs
@@ -80,8 +80,15 @@ for (var i = 0; i < args.Length; i++)
 app.Urls.Clear();
 app.Urls.Add($"http://127.0.0.1:{port}");
 
-app.Lifetime.ApplicationStarted.Register(() =>
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
+    // Start gRPC server if --grpc flag
+    if (grpcArgs.Contains("--grpc"))
+    {
+        var grpc = app.Services.GetRequiredService<SidecarGrpcServer>();
+        _ = grpc.StartAsync(app.Lifetime.ApplicationStopped);
+    }
+
     var mode = sidecarMode;
     var auth = !string.IsNullOrEmpty(builder.Configuration.GetValue<string>("Sidecar:Auth:Token"));
     var proxy = !string.IsNullOrEmpty(builder.Configuration.GetValue<string>("Sidecar:KernelApi:BaseUrl"));
